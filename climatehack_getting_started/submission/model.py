43086a628e15ac23c0713301da637e8de10a6e94
NameeraAzim <55632835+NameeraAzim@users.noreply.github.com>
2022-03-14 02:37:20 -0400

    Committing submission folder

---
+++ climatehack_getting_started/submission/model.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.layer1 = nn.Conv2d(12, 24, kernel_size=3, padding=1)
+        self.pool1 = nn.AvgPool2d(stride=2, kernel_size=3, padding=1)
+
+        self.layer2 = nn.Conv2d(24, 48, kernel_size=3, padding=1)
+        self.pool2 = nn.AvgPool2d(stride=2, kernel_size=3, padding=1)
+
+        self.layer3 = nn.Conv2d(48, 96, kernel_size=3, padding=1)
+        self.pool3 = nn.AvgPool2d(stride=2, kernel_size=3, padding=1)
+
+        self.layer5 = nn.ConvTranspose2d(96, 48, kernel_size=2, stride=2)
+        self.layer7 = nn.ConvTranspose2d(48, 24, kernel_size=2, stride=2)
+
+    def forward(self, x):
+        x = x / 1023.0
+
+        x = self.layer1(x)
+        x = torch.relu(self.pool1(x))
+
+        x = self.layer2(x)
+        x = torch.relu(self.pool2(x))
+
+        x = self.layer3(x)
+        x = torch.relu(self.pool3(x))
+
+        # if you want to experiment with a dense layer on the "latent space"
+        # between the encoder and decoder parts, here's where you'd put it!
+
+        x = torch.relu(self.layer5(x.view(-1, 96, 16, 16)))
+        x = torch.sigmoid(self.layer7(x)) * 1023.0
+
+        return x
